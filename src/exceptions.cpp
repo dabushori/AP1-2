855192af6b37fbe6ed87a101516e4e56e13251ef
Ori Dabush <dabushori@gmail.com>
2020-08-20 15:46:09 +0300

    Created ErrorCodeException class

---
+++ src/exceptions.cpp
@@ -0,0 +1,19 @@
+#include "ErrorCode.h"
+#include "Matrix.h"
+#include <exception>
+
+namespace exceptions
+{
+    class ErrorCodesException : public std::exception
+    {
+        ErrorCode code;
+
+    public:
+        ErrorCodesException(const ErrorCode code) { this->code = code; }
+
+        const char *what() const noexcept override
+        {
+            return error_getErrorMessage(code);
+        }
+    };
+}; // namespace exceptions
