12c52d48bb866e7c595ceee8b4b9ddc98439daa6
Ori Dabush <dabushori@gmail.com>
2020-08-20 18:22:08 +0300

    Added multiplyMatrixByScalar method

--- src/Mat.cpp
+++ src/Mat.cpp
@@ -1,85 +1,93 @@
-#include "Matrix.h"
 #include "Mat.hpp"
+#include "Matrix.h"
 namespace matrix {
-    Mat::Mat(const uint32_t height, const uint32_t width) {
-        ErrorCode code = matrix_create(&this->m_matrix, height, width);
-        if (!error_isSuccess(code)) {
-        throw exceptions::ErrorCodesException(code);
-        }
-    }
-    Mat::Mat(const Mat& other) {
-        ErrorCode code = matrix_copy(&this->m_matrix, other.m_matrix);
-        if (!error_isSuccess(code)) {
-        throw exceptions::ErrorCodesException(code);
-        }
-    }
-    Mat& Mat::operator=(const Mat& other) {
-        if (this == &other) {
-		    return *this;
-        }
-	    delete m_matrix;
-	    *this = Mat(other); 
-	    return *this;
-    }
-    Mat::Mat(Mat&& other) noexcept {
-        m_matrix = std::exchange(other.m_matrix, nullptr);
-    }
-    Mat& Mat::operator=(Mat&& other) noexcept {
-        if (this == &other) {
-		return *this;
-	    }
-	    matrix_destroy(this->m_matrix);
-	    m_matrix = std::exchange(other.m_matrix, nullptr);
-	    return *this;
-    }
-    Mat::~Mat() {
-        matrix_destroy(this->m_matrix);
-    }
-    uint32_t Mat::getHeight() const {
-        uint32_t result;
-        ErrorCode code = matrix_getHeight(this->m_matrix, &result);
-        if (!error_isSuccess(code)) {
-        throw exceptions::ErrorCodesException(code);
-        }
-        return result;
-    }
-    uint32_t Mat::getWidth() const {
-        uint32_t result;
-        ErrorCode code = matrix_getWidth(this->m_matrix, &result);
-        if (!error_isSuccess(code)) {
-        throw exceptions::ErrorCodesException(code);
-        }
-        return result;
-    }
-    void Mat::setValue(const uint32_t rowIndex, const uint32_t colIndex, const double value) {
-        ErrorCode code = matrix_setValue(this->m_matrix, rowIndex, colIndex, value);
-        if (!error_isSuccess(code)) {
-        throw exceptions::ErrorCodesException(code);
-        }
-    }
-    Mat Mat::add(const Mat& other) const {
-        Mat matrix(this->getHeight(), this->getWidth());
-        ErrorCode code = matrix_add(&matrix.m_matrix, this->m_matrix, other.m_matrix);
-        if (!error_isSuccess(code)) {
-            throw exceptions::ErrorCodesException(code);
-        }
-        return matrix;
-    }
-    Mat Mat::multiplyMatrices(const Mat& other) const {
-        Mat matrix(this->getHeight(), this->getWidth());
-        ErrorCode code = matrix_multiplyMatrices(&matrix.m_matrix, this->m_matrix, other.m_matrix);
-        if (!error_isSuccess(code)) {
-            throw exceptions::ErrorCodesException(code);
-        }
-        return matrix;
-    }
-    double Mat::operator()(const uint32_t rowIndex, const uint32_t colIndex) {
-        double result;
-        ErrorCode code = matrix_getValue(this->m_matrix, rowIndex, colIndex, &result);
-        if (!error_isSuccess(code)) {
-            throw exceptions::ErrorCodesException(code);
-        }
-        return result;
-    }
-
+Mat::Mat(const uint32_t height, const uint32_t width) {
+  ErrorCode code = matrix_create(&this->m_matrix, height, width);
+  if (!error_isSuccess(code)) {
+    throw exceptions::ErrorCodesException(code);
+  }
 }
+Mat::Mat(const Mat &other) {
+  ErrorCode code = matrix_copy(&this->m_matrix, other.m_matrix);
+  if (!error_isSuccess(code)) {
+    throw exceptions::ErrorCodesException(code);
+  }
+}
+Mat &Mat::operator=(const Mat &other) {
+  if (this == &other) {
+    return *this;
+  }
+  delete m_matrix;
+  *this = Mat(other);
+  return *this;
+}
+Mat::Mat(Mat &&other) noexcept {
+  m_matrix = std::exchange(other.m_matrix, nullptr);
+}
+Mat &Mat::operator=(Mat &&other) noexcept {
+  if (this == &other) {
+    return *this;
+  }
+  matrix_destroy(this->m_matrix);
+  m_matrix = std::exchange(other.m_matrix, nullptr);
+  return *this;
+}
+Mat::~Mat() { matrix_destroy(this->m_matrix); }
+uint32_t Mat::getHeight() const {
+  uint32_t result;
+  ErrorCode code = matrix_getHeight(this->m_matrix, &result);
+  if (!error_isSuccess(code)) {
+    throw exceptions::ErrorCodesException(code);
+  }
+  return result;
+}
+uint32_t Mat::getWidth() const {
+  uint32_t result;
+  ErrorCode code = matrix_getWidth(this->m_matrix, &result);
+  if (!error_isSuccess(code)) {
+    throw exceptions::ErrorCodesException(code);
+  }
+  return result;
+}
+void Mat::setValue(const uint32_t rowIndex, const uint32_t colIndex,
+                   const double value) {
+  ErrorCode code = matrix_setValue(this->m_matrix, rowIndex, colIndex, value);
+  if (!error_isSuccess(code)) {
+    throw exceptions::ErrorCodesException(code);
+  }
+}
+Mat Mat::add(const Mat &other) const {
+  Mat matrix(this->getHeight(), this->getWidth());
+  ErrorCode code = matrix_add(&matrix.m_matrix, this->m_matrix, other.m_matrix);
+  if (!error_isSuccess(code)) {
+    throw exceptions::ErrorCodesException(code);
+  }
+  return matrix;
+}
+Mat Mat::multiplyMatrices(const Mat &other) const {
+  Mat matrix(this->getHeight(), this->getWidth());
+  ErrorCode code =
+      matrix_multiplyMatrices(&matrix.m_matrix, this->m_matrix, other.m_matrix);
+  if (!error_isSuccess(code)) {
+    throw exceptions::ErrorCodesException(code);
+  }
+  return matrix;
+}
+Mat Mat::multiplyByScalar(const double scalar) const {
+  Mat matrix(this->getHeight(), this->getWidth());
+  ErrorCode code = matrix_multiplyWithScalar(this->m_matrix, scalar);
+  if (!error_isSuccess(code)) {
+    throw exceptions::ErrorCodesException(code);
+  }
+  return matrix;
+}
+double Mat::operator()(const uint32_t rowIndex, const uint32_t colIndex) {
+  double result;
+  ErrorCode code = matrix_getValue(this->m_matrix, rowIndex, colIndex, &result);
+  if (!error_isSuccess(code)) {
+    throw exceptions::ErrorCodesException(code);
+  }
+  return result;
+}
+
+} // namespace matrix
--- src/Mat.hpp
+++ src/Mat.hpp
@@ -1,37 +1,41 @@
 #pragma once
-#include <stdint.h>
-#include <utility>
 #include "Matrix.h"
 #include "exceptions.hpp"
+#include <stdint.h>
+#include <utility>
 namespace matrix {
-//Our matrix class
+// Our matrix class
 class Mat {
 public:
-    // constructor
-    Mat(const uint32_t height, const uint32_t width);
-    // copy constructor
-    Mat(const Mat& other);
-    // copy assignment operator
-    Mat& operator=(const Mat& other);
-    // Move Constructor
-    Mat(Mat&& other) noexcept;
-    // Move assignment operator
-    Mat& operator=(Mat&& other) noexcept;
-    // destructor
-    ~Mat();
-    // get Height
-    uint32_t getHeight() const;
-    // get Width
-    uint32_t getWidth() const;
-    // set the value in i and j index
-    void setValue(const uint32_t rowIndex, const uint32_t colIndex, const double value);
-    // add matrix to this matrix
-    Mat add(const Mat& other) const;
-    // multiply another matrix with this matrix
-    Mat multiplyMatrices(const Mat& other) const;
-    // get the value in the rowIndex and the colIndex of the Mat
-    double operator()(const uint32_t rowIndex, const uint32_t colIndex);
+  // constructor
+  Mat(const uint32_t height, const uint32_t width);
+  // copy constructor
+  Mat(const Mat &other);
+  // copy assignment operator
+  Mat &operator=(const Mat &other);
+  // Move Constructor
+  Mat(Mat &&other) noexcept;
+  // Move assignment operator
+  Mat &operator=(Mat &&other) noexcept;
+  // destructor
+  ~Mat();
+  // get Height
+  uint32_t getHeight() const;
+  // get Width
+  uint32_t getWidth() const;
+  // set the value in i and j index
+  void setValue(const uint32_t rowIndex, const uint32_t colIndex,
+                const double value);
+  // add matrix to this matrix
+  Mat add(const Mat &other) const;
+  // multiply another matrix with this matrix
+  Mat multiplyMatrices(const Mat &other) const;
+  // multiply this matrix by a scalar
+  Mat multiplyByScalar(const double scalar) const;
+  // get the value in the rowIndex and the colIndex of the Mat
+  double operator()(const uint32_t rowIndex, const uint32_t colIndex);
+
 private:
-    Matrix* m_matrix;
+  Matrix *m_matrix;
 };
-}
+} // namespace matrix
