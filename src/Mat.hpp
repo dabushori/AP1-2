e5298b29c9c93931daefac7044e05c01586d2ff7
Ori Dabush <dabushori@gmail.com>
2020-08-21 12:48:50 +0300

    Added documentaion

--- src/Mat.hpp
+++ src/Mat.hpp
@@ -4,48 +4,109 @@
 #include "exceptions.hpp"
 #include <stdint.h>
 #include <utility>
+
 namespace matrix {
-// Our matrix class
 class Mat {
 public:
-  // constructor
+  /**
+   * @brief Construct a new Mat object with size height x width
+   *
+   * @param height the height of the matrix
+   * @param width the width of the matrix
+   */
   Mat(const uint32_t height, const uint32_t width);
 
-  // copy constructor
+  /**
+   * @brief copy constructor
+   *
+   * @param other the other Mat object that is being copied
+   */
   Mat(const Mat &other);
 
-  // copy assignment operator
+  /**
+   * @brief copy assignment operator
+   *
+   * @param other the other Mat object that is being assigned
+   * @return Mat& the new Mat object that was created
+   */
   Mat &operator=(const Mat &other);
 
-  // Move Constructor
+  /**
+   * @brief move constructor
+   *
+   * @param other the other Mat object that is being moved
+   */
   Mat(Mat &&other) noexcept;
 
-  // Move assignment operator
+  /**
+   * @brief move assignment operator
+   *
+   * @param other the other Mat object that is being assigned
+   * @return Mat& the new Mat object that was created
+   */
   Mat &operator=(Mat &&other) noexcept;
 
-  // destructor
+  /**
+   * @brief Destroy the Mat object
+   *
+   */
   ~Mat();
 
-  // get Height
+  /**
+   * @brief Get the height of the matrix
+   *
+   * @return uint32_t the height of the matrix
+   */
   uint32_t getHeight() const;
 
-  // get Width
+  /**
+   * @brief Get the width of the matrix
+   *
+   * @return uint32_t the width of the matrix
+   */
   uint32_t getWidth() const;
 
-  // set the value in i and j index
+  /**
+   * @brief Set the value of the cell in location i,j
+   *
+   * @param rowIndex the row index of the cell
+   * @param colIndex the collumn index of the cell
+   * @param value the value that is being set
+   */
   void setValue(const uint32_t rowIndex, const uint32_t colIndex,
                 const double value);
 
-  // add matrix to this matrix
+  /**
+   * @brief add the current matrix and a given matrix
+   *
+   * @param other the given matrix
+   * @return Mat the result of the addition
+   */
   Mat add(const Mat &other) const;
 
-  // multiply another matrix with this matrix
+  /**
+   * @brief multiply the current matrix and a given matrix
+   *
+   * @param other the given matrix
+   * @return Mat the result of the multiplication
+   */
   Mat multiplyMatrices(const Mat &other) const;
 
-  // multiply this matrix by a scalar
+  /**
+   * @brief multiply the current matrix by a given scalar
+   *
+   * @param scalar the given scalar
+   * @return Mat the result of the multiplication
+   */
   Mat multiplyByScalar(const double scalar) const;
 
-  // get the value in the rowIndex and the colIndex of the Mat
+  /**
+   * @brief get the value in the rowIndex,colIndex cell of the current matrix
+   *
+   * @param rowIndex the row index of the wanted cell
+   * @param colIndex the collumn index of the wanted cell
+   * @return double the value that is in the cell
+   */
   double operator()(const uint32_t rowIndex, const uint32_t colIndex) const;
 
 private:
--- src/exceptions.hpp
+++ src/exceptions.hpp
@@ -10,7 +10,18 @@ class ErrorCodesException : public std::exception {
   ErrorCode code;
 
 public:
+  /**
+   * @brief Construct a new Error Codes Exception object using a given ErrorCode
+   *
+   * @param code the given ErrorCode that is thrown
+   */
   ErrorCodesException(const ErrorCode code);
+
+  /**
+   * @brief gets the error message
+   *
+   * @return const char* the error message
+   */
   const char *what() const noexcept override;
 };
 } // namespace exceptions
